4ee2a56be2c5a0ca9f0c05047ca6283f4e2b0723
sunnyy02 <sunnyy@gmail.com>
2023-07-08 14:46:56 +1000

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
@@ -6,6 +7,15 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  
+  const options = new DocumentBuilder()
+    .setTitle('Address Book API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 
   if (module.hot) {

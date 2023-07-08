47a1ae4b41ebfbb762f640505c50155b28d000ff
sunnyy02 <sunnyy@gmail.com>
2023-07-08 14:19:03 +1000

    add dtos

--- src/address-book/address.service.ts
+++ src/address-book/address.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@nestjs/common";
-import { AddressDto } from "./address-dto";
+import { AddressDto } from "./address.dto";
 
 @Injectable()
 export class AddressService {
     private addressDataStore: AddressDto[] = [{
         id: 1,
         addressLine: '123 Queen street',
-        postCode: '4000',
-        state: 'QLD'
+        postCode: 4000,
+        state: 'QLD',
+        createdDate: new Date()
     }];
     
     getById(id: number) {
---
+++ src/address-book/create-address.dto.ts
@@ -0,0 +1,4 @@
+import { OmitType } from "@nestjs/mapped-types";
+import { AddressDto } from "./address.dto";
+
+export class CreateAddressDto extends OmitType(AddressDto, ['id', 'createdDate']) { }

56f670de071dd5f9c66c634de7f799cbab817dad
sunnyy02 <sunnyy@gmail.com>
2023-09-03 16:41:15 +1000

    enabled serialization

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Post, UsePipes } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Get, NotFoundException, Param, ParseIntPipe, Post, SerializeOptions, UseInterceptors, UsePipes } from '@nestjs/common';
 import { CreateContactDto } from './create-contact.dto';
 import { CreateUsersDto } from './create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
+@UseInterceptors(ClassSerializerInterceptor)
+@SerializeOptions({})
 export class UserController {
     constructor(private readonly userService: UserService){
 

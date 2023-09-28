74d22b89585c5f93ac1227b816e77e2801bf03e5
sunnyy02 <sunnyy@gmail.com>
2023-09-28 10:40:13 +1000

    reformat

--- src/address-book/address-dto.ts
+++ src/address-book/address-dto.ts
@@ -1,7 +1,7 @@
 export class AddressDto {
-    id: number;
-    addressLine: string;
-    postCode: number;
-    state: string;
-    createdDate: Date;
+  id: number;
+  addressLine: string;
+  postCode: number;
+  state: string;
+  createdDate: Date;
 }

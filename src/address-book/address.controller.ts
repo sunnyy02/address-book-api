717a157a53f54eb052726ad54d1595ae9804517a
sunnyy02 <sunnyy@gmail.com>
2023-09-30 09:04:33 +1000

    clean up

--- src/address-book/address.controller.ts
+++ src/address-book/address.controller.ts
@@ -3,10 +3,10 @@ import { AddressService } from './address.service';
 
 @Controller('address')
 export class AddressController {
-    constructor(private readonly addressService: AddressService) {}
+  constructor(private readonly addressService: AddressService) {}
 
-    @Get(':id')
-    getById(@Param('id', ParseIntPipe) id: number) {
-        return this.addressService.getById(id);
-    }
+  @Get(':id')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return this.addressService.getById(id);
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AddressBookModule } from './address-book/address-book.module';
 
 @Module({
   imports: [AddressBookModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

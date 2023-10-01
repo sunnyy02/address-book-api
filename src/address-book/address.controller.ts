e4b374c1b889ea780cd8a62991c047fdde62360b
sunnyy02 <sunnyy@gmail.com>
2023-10-01 16:44:34 +1000

    access parameters

--- src/address-book/address.controller.ts
+++ src/address-book/address.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { AddressDto } from './address-dto';
 import { AddressService } from './address.service';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
-
+  // Get a single route parameter
   @Get(':id')
   getById(@Param('id', ParseIntPipe) id: number) {
-    return this.addressService.getById(id);
+    console.log('id paramter:', id);
+    return new AddressDto();
+  }
+
+  // Option 1: Use @Param as an object
+  @Get('/country/:country/postcode/:postcode')
+  search(@Param() params: { country: string; postcode: string }) {
+    console.log('country', params.country);
+    console.log('postcode', params.postcode);
+  }
+
+  // Option 2: multiple @Param
+  //   @Get('/country/:country/postcode/:postcode')
+  //   search2(@Param('country') country: string, @Param('postcode') postcode: string) {
+  //       console.log("country", country);
+  //       console.log("postcode", postcode);
+  //   }
+
+  // Query parameters#
+  @Get()
+  searchByPostCode(@Query('postcode') postcode: string) {
+    console.log(postcode);
+  }
+
+  // Access the request body using the @Body()decorator
+  @Post()
+  create(@Body() address: AddressDto) {
+    console.log('AddressDto:', address);
   }
 }

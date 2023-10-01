45ccdc23e3fad66e921a44221f8aa92509c9daa5
sunnyy02 <sunnyy@gmail.com>
2023-10-02 08:51:06 +1000

    clean up

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {

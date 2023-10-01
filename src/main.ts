aac7a19d8a1d481b95704b12ebb8396f03a85a4f
sunnyy02 <sunnyy@gmail.com>
2023-10-02 08:20:39 +1000

    fix the docker issue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors(); 
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  // When running fastify inside a docker, we need to specify the host, which should be '0.0.0.0'
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();

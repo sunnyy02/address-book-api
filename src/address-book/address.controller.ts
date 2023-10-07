434d457725ab2850dc3bf41a7d1fe63dd6bdd493
sunnyy02 <sunnyy@gmail.com>
2023-10-07 16:58:19 +1000

    add API descriptions

--- src/address-book/address.controller.ts
+++ src/address-book/address.controller.ts
@@ -8,35 +8,53 @@ import {
   Put,
   Delete,
 } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { AddressDto } from './address.dto';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './create-address.dto';
 
+@ApiTags('Address')
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get an address by id' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'Address id',
+    type: Number
+  })
+  @ApiOkResponse({ description: 'The Address was retrieved successfully' })
   getById(@Param('id', ParseIntPipe) id: number) {
     return this.addressService.getById(id);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all addresses' })
+  @ApiOkResponse({ description: 'Addresses were returned successfully' })
   getAll() {
     return this.addressService.getAll();
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create an address' })
+  @ApiCreatedResponse({ description: 'Address Created Succesfully' })
   create(@Body() address: CreateAddressDto) {
     return this.addressService.create(address);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update an address' })
+  @ApiOkResponse({ description: 'The address was updated successfully' })
   update(@Param('id', ParseIntPipe) id: number, @Body() address: AddressDto) {
     return this.addressService.update(id, address);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete an address' })
+  @ApiOkResponse({ description: 'The address was deleted successfully' })
   deleteById(@Param('id', ParseIntPipe) id: number) {
     return this.addressService.delete(id);
   }
--- src/address-book/address.dto.ts
+++ src/address-book/address.dto.ts
@@ -1,7 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class AddressDto {
     id: number;
+    @ApiProperty({
+        type: String,
+        description: 'Address line',
+      })
     addressLine: string;
+    
+    @ApiProperty({
+        type: String,
+        description: 'Post code',
+      })
     postCode: number;
+
+    @ApiProperty({
+        type: String,
+        description: 'State',
+      })
     state: string;
+
+    @ApiProperty({
+        type: String,
+        description: 'Created date time',
+      })
     createdDate: Date;
 }

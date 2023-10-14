4f6ec2ae17a390cb14350c23516fc63a925a780a
sunnyy02 <sunnyy@gmail.com>
2023-10-14 20:32:36 +1000

    clean up

--- src/address-book/address.controller.ts
+++ src/address-book/address.controller.ts
@@ -1,4 +1,16 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Put, Delete, ParseArrayPipe, UsePipes, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Delete,
+  ParseArrayPipe,
+  UsePipes,
+  Query,
+} from '@nestjs/common';
 import { AddressIdParam } from './address-id-param';
 import { AddressDto } from './address.dto';
 import { AddressService } from './address.service';
@@ -6,39 +18,39 @@ import { CreateAddressDto } from './create-address.dto';
 
 @Controller('address')
 export class AddressController {
-    constructor(private readonly addressService: AddressService) {}
+  constructor(private readonly addressService: AddressService) {}
 
-    @Get(':id')
-    getById(@Param('id', ParseIntPipe) id: number) {
-        return this.addressService.getById(id);
-    }
+  @Get(':id')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return this.addressService.getById(id);
+  }
 
-    // address?id=1
-    @Get()
-    search(@Query() idParam: AddressIdParam) {
-        return this.addressService.getById(idParam.id);
-    }
+  // address?id=1
+  @Get()
+  search(@Query() idParam: AddressIdParam) {
+    return this.addressService.getById(idParam.id);
+  }
 
-    @Post()
-    create(@Body() address: CreateAddressDto) {
-        return this.addressService.create(address);
-    }
+  @Post()
+  create(@Body() address: CreateAddressDto) {
+    return this.addressService.create(address);
+  }
 
-    @Post()
-    createAddressByBatch(
+  @Post()
+  createAddressByBatch(
     @Body(new ParseArrayPipe({ items: CreateAddressDto }))
     createAddressDtos: CreateAddressDto[],
-    ) {
-        // implement the create a batch of addresses
-    }
+  ) {
+    // implement the create a batch of addresses
+  }
 
-    @Put(':id')
-    update(@Param() idParam: AddressIdParam, @Body() address: AddressDto) {
-        return this.addressService.update(idParam.id, address);
-    }
+  @Put(':id')
+  update(@Param() idParam: AddressIdParam, @Body() address: AddressDto) {
+    return this.addressService.update(idParam.id, address);
+  }
 
-    @Delete(':id')
-    deleteById(@Param() idParam: AddressIdParam) {
-        return this.addressService.delete(idParam.id);
-    }
+  @Delete(':id')
+  deleteById(@Param() idParam: AddressIdParam) {
+    return this.addressService.delete(idParam.id);
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AddressBookModule } from './address-book/address-book.module';
 
 @Module({
   imports: [AddressBookModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

339dc79272c776437460ed89b4672d2b73bb2411
sunnyy02 <sunnyy@gmail.com>
2023-10-25 09:27:31 +1000

    clean up

--- src/user/create-contact.dto.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType } from "@nestjs/mapped-types";
-import { ContactDto } from "./contact.dto";
-
-export class CreateContactDto extends OmitType(ContactDto, ['id' ]) { }
--- src/user/create-role.dto.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType } from "@nestjs/mapped-types";
-import { RoleDto } from "./role.dto";
-
-export class CreateRoleDto extends OmitType(RoleDto, ['id' ]) { }

fc8bf9618f03f8bdd135f601f1b3c4c8fb10c055
sunnyy02 <sunnyy@gmail.com>
2023-10-26 08:07:17 +1000

    update response

--- src/common/response.interceptor.ts
+++ src/common/response.interceptor.ts
@@ -1,12 +1,22 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CallHandler, ExecutionContext, HttpException, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable, catchError, map, throwError } from 'rxjs';
 import { v4 as uuid} from 'uuid';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const ResponseObj = context.switchToHttp().getResponse();
-    ResponseObj.setHeader('Etag', uuid());
-    return next.handle();
+
+    return next.handle().pipe(
+      map((res: unknown) => {
+        const resObj = context.switchToHttp().getResponse();
+        resObj.setHeader('Etag', uuid());
+        return {
+          data: res,
+          status: resObj.statusCode,
+          success: true,
+          error: null
+        }
+      })
+    );
   }
 }

493757bc46acad77de30d46d2b168e07ef93578d
sunnyy02 <sunnyy@gmail.com>
2023-11-05 22:02:37 +1000

    adjust roles

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -7,27 +7,35 @@ import { RoleGuard } from 'src/auth/role.guard';
 import { Roles } from '../common/role.decorator';
 import { Role } from '../common/role.enum';
 
-@Roles(Role.Writer)
-@UseGuards(JwtAuthGuard, RoleGuard)
+@UseGuards(JwtAuthGuard)
 @Controller('address')
 export class AddressController {
     constructor(private readonly addressService: AddressService) {}
 
+    @Roles(Role.Reader, Role.Writer,Role.Admin)
+    @UseGuards(RoleGuard)
     @Get(':id')
     getById(@Param('id', ParseIntPipe) id: number) {
         return this.addressService.getById(id);
     }
 
+    @Roles(Role.Writer,Role.Admin)
+    @UseGuards(RoleGuard)
     @Post()
     create(@Body() address: CreateAddressDto) {
         return this.addressService.create(address);
     }
 
+    @Roles(Role.Writer,Role.Admin)
+    @UseGuards(RoleGuard)
+    @Post()
     @Put(':id')
     update(@Param('id', ParseIntPipe) id: number, @Body() address: AddressDto) {
         return this.addressService.update(id, address);
     }
 
+    @Roles(Role.Admin)
+    @UseGuards(RoleGuard)
     @Delete(':id')
     deleteById(@Param('id', ParseIntPipe) id: number) {
         return this.addressService.delete(id);

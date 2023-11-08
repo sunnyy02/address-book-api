a09bb0ec5d824acbc30715cb5eb5c960f38d9e15
sunnyy02 <sunnyy@gmail.com>
2023-11-08 20:42:56 +1000

    clean up

--- src/address-book/address.controller.ts
+++ src/address-book/address.controller.ts
@@ -13,19 +13,23 @@ import { AddressDto } from './address.dto';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './create-address.dto';
 
+// Assign the Address tag to the controller
 @ApiTags('Address')
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
   @Get(':id')
+  // Add the description to the GetById controller action.
   @ApiOperation({ summary: 'Get an address by id' })
+  // Specify information about id parameter
   @ApiParam({
     name: 'id',
     required: true,
     description: 'Address id',
     type: Number
   })
+  // Defines a successful response for the GetbyId API documentation.
   @ApiOkResponse({ description: 'The Address was retrieved successfully' })
   getById(@Param('id', ParseIntPipe) id: number) {
     return this.addressService.getById(id);
--- src/address-book/address.dto.ts
+++ src/address-book/address.dto.ts
@@ -1,28 +1,29 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AddressDto {
-    id: number;
-    @ApiProperty({
-        type: String,
-        description: 'Address line',
-      })
-    addressLine: string;
-    
-    @ApiProperty({
-        type: String,
-        description: 'Post code',
-      })
-    postCode: number;
+  id: number;
+  // Provide additional information of addressLine property for the API documentation
+  @ApiProperty({
+    type: String,
+    description: 'Address line',
+  })
+  addressLine: string;
 
-    @ApiProperty({
-        type: String,
-        description: 'State',
-      })
-    state: string;
+  @ApiProperty({
+    type: String,
+    description: 'Post code',
+  })
+  postCode: number;
 
-    @ApiProperty({
-        type: String,
-        description: 'Created date time',
-      })
-    createdDate: Date;
+  @ApiProperty({
+    type: String,
+    description: 'State',
+  })
+  state: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Created date time',
+  })
+  createdDate: Date;
 }

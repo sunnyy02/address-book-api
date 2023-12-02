04d48cf5616f42083cce40a1ee6c4f2cb5376da9
sunnyy02 <sunnyy@gmail.com>
2023-12-02 20:28:34 +1000

    reformat

--- src/typeorm.ts
+++ src/typeorm.ts
@@ -6,7 +6,8 @@ import { ContactEntity } from "./common/entities/contact.entity";
 import { RoleEntity } from "./common/entities/role.entity";
 import { StateEntity } from "./common/entities/state.entity";
 import { UserEntity } from "./common/entities/user.entity";
-import { InitDatabase1692783883190 } from "./migration/1692783883190-InitDatabase";
+import { InitDatabase1692783883190 } 
+        from "./migration/1692783883190-InitDatabase";
 
 
 dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });

f66ca8bd56d42b9cc473021e68a20b8b47baaf51
sunnyy02 <sunnyy@gmail.com>
2023-12-02 19:11:11 +1000

    clean up

--- src/typeorm.ts
+++ src/typeorm.ts
@@ -6,6 +6,8 @@ import { ContactEntity } from "./common/entities/contact.entity";
 import { RoleEntity } from "./common/entities/role.entity";
 import { StateEntity } from "./common/entities/state.entity";
 import { UserEntity } from "./common/entities/user.entity";
+import { InitDatabase1701507773652 } from "./migrations/1701507773652-InitDatabase";
+import { Test1701508081291 } from "./migrations/1701508081291-test";
 
 
 dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
@@ -18,7 +20,8 @@ const config = {
     password: `${process.env.DATABASE_PASSWORD}`,
     database: `${process.env.DATABASE_NAME}`,
     autoLoadEntities: true,
-    synchronize: false, //`${process.env.SYNCHRONIZE}`,
+    synchronize: `${process.env.SYNCHRONIZE}` === 'true',
+    // we set it to false in the .env.dev file
   };
 
   const {autoLoadEntities,synchronize ,...configWithAutoLoad} = config;

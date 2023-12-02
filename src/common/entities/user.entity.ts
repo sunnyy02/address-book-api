c3c2a30861a0580ec18dc379524a8fbd5501efef
sunnyy02 <sunnyy@gmail.com>
2023-12-03 09:16:51 +1000

    add comments

--- src/common/entities/user.entity.ts
+++ src/common/entities/user.entity.ts
@@ -1,30 +1,31 @@
-import { Exclude, Expose } from 'class-transformer';
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Expose } from 'class-transformer';
 import { RoleConstant } from './role.constant';
 
 @Entity('user')
 export class UserEntity {
+  // Primary key column, exposed to Admins, Editors, and Readers
   @PrimaryGeneratedColumn()
-  @Expose({groups: [RoleConstant.Admin, RoleConstant.Editor, RoleConstant.Reader]})
+  @Expose({ groups: [RoleConstant.Admin, RoleConstant.Editor, RoleConstant.Reader] })
   id: number;
 
+  // User name column, exposed to Admins, Editors, and Readers
   @Column()
-  @Expose({groups: [RoleConstant.Admin, RoleConstant.Editor, RoleConstant.Reader]})
+  @Expose({ groups: [RoleConstant.Admin, RoleConstant.Editor, RoleConstant.Reader] })
   user_name: string;
 
+  // Email column, exposed to Admins, Editors, and Readers
   @Column()
-  @Expose({groups: [RoleConstant.Admin, RoleConstant.Editor, RoleConstant.Reader]})
+  @Expose({ groups: [RoleConstant.Admin, RoleConstant.Editor, RoleConstant.Reader] })
   email: string;
 
-  @Column({default: 'NA'})
-  @Expose({groups: [RoleConstant.Admin, RoleConstant.Editor]})
+  // Pay grade column, exposed to Admins and Editors only
+  @Column({ default: 'NA' })
+  @Expose({ groups: [RoleConstant.Admin, RoleConstant.Editor] })
   pay_grade: string;
 
+  // Password column, exposed to Admins only
   @Column()
-  @Expose({groups: [RoleConstant.Admin]})
+  @Expose({ groups: [RoleConstant.Admin] })
   password: string;
 }

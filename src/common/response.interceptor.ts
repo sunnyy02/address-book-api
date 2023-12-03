efaece9f412cdedbe53a8d23e508a6df03f07320
sunnyy02 <sunnyy@gmail.com>
2023-12-03 13:16:00 +1000

    add null check

--- src/common/response.interceptor.ts
+++ src/common/response.interceptor.ts
@@ -1,4 +1,10 @@
-import { CallHandler, ExecutionContext, HttpException, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable, catchError, map, throwError } from 'rxjs';
 import { convertCamel } from './convert-camel';
 
@@ -6,19 +12,25 @@ import { convertCamel } from './convert-camel';
 export class ResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const reqObj = context.switchToHttp().getRequest();
-    console.log(reqObj.body,'=>', convertCamel(reqObj.body))
-    reqObj.body = convertCamel(reqObj.body);
 
-    return next.handle().pipe(
-      map((res: unknown) => {
-        const resObj = context.switchToHttp().getResponse();
-        return {
-          data: res,
-          status: resObj.statusCode,
-          success: true,
-          error: null
-        }
-      })
-    );
+    if (reqObj && reqObj.body) {
+      console.log(reqObj.body, '=>', convertCamel(reqObj.body));
+      reqObj.body = convertCamel(reqObj.body);
+
+      return next.handle().pipe(
+        map((res: unknown) => {
+          const resObj = context.switchToHttp().getResponse();
+          return {
+            data: res,
+            status: resObj.statusCode,
+            success: true,
+            error: null,
+          };
+        }),
+      );
+    }
+
+    // If request or its body is not present, proceed without modification
+    return next.handle();
   }
 }
